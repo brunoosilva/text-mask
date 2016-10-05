34dc60a13847726717de50487ad2c74f49fb6e72
Bruno Oliveira <321.bruno@gmail.com>
2016-10-05 01:09:17 -0300

    Ajust test for support AoT

--- angular2/test/angular2TextMask.spec.js
+++ angular2/test/angular2TextMask.spec.js
@@ -3,9 +3,9 @@ require('ts-node').register({project: './angular2/'}) // Allows us to load a Typ
 import 'reflect-metadata'
 import packageJson from '../package.json'
 
-const MaskedInput = (isVerify()) ?
-  require(`../${packageJson.main}`).default :
-  require('../src/angular2TextMask.ts').default
+const MaskedInputDirective = (isVerify()) ?
+  require(`../${packageJson.main}`).MaskedInputDirective :
+  require('../src/angular2TextMask.ts').MaskedInputDirective
 
 describe('MaskedInput', () => {
   let inputElement
@@ -19,7 +19,7 @@ describe('MaskedInput', () => {
 
   it('does not throw when instantiated', () => {
     expect(() => {
-      const maskedInput = new MaskedInput(renderer, {nativeElement: inputElement}, ngModel)
+      const maskedInput = new MaskedInputDirective(renderer, {nativeElement: inputElement}, ngModel)
 
       maskedInput.ngOnInit({mask: '(111)'})
 
